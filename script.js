d1990e11452ec33186ca671cace0d3fa9aa50a4c
ssawalwade9 <shruti1461@gmail.com>
2025-04-01 22:13:50 +0530

    donee

--- script.js
+++ script.js
@@ -9,7 +9,18 @@ arr.forEach(button =>{
         string = eval(string);
         input.value = string;
         }
+        else if(e.target.innerHTML== 'AC'){
+            string= "";
+            input.value = string
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length-1);
+            input.value = string;
+        }
+        else{
         string += e.target.innerHTML;
         input.value = string;
-    })
+        }
+    }
+)
 })
